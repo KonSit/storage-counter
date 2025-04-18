f7e095ea99cdc0d98036b228a06f3890fdc928d4
KonSit <konsitness@gmail.com>
2025-04-18 22:41:40 +0300

    inventory to map & button

--- src/main/java/net/pacifickid/storagecounter/StorageCounterClient.java
+++ src/main/java/net/pacifickid/storagecounter/StorageCounterClient.java
@@ -2,12 +2,21 @@ package net.pacifickid.storagecounter;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.text.Text;
 
 public class StorageCounterClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
             System.out.println("Открыт экран: " + screen.getClass().getName());
+        });
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(CommandManager.literal("countaround").executes(context -> {
+                context.getSource().sendFeedback(() -> Text.literal("Called /countaround."), false);
+                return 1;
+            }));
         });
     }
 }
--- src/main/java/net/pacifickid/storagecounter/mixin/ChestScreenMixin.java
+++ src/main/java/net/pacifickid/storagecounter/mixin/ChestScreenMixin.java
@@ -3,26 +3,31 @@ package net.pacifickid.storagecounter.mixin;
 import net.fabricmc.fabric.api.client.screen.v1.Screens;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.text.MutableText;
+import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.text.Text;
+import net.pacifickid.storagecounter.StorageCounter;
+import net.pacifickid.storagecounter.chestinteraction.ChestInventoryInteraction;
 import org.spongepowered.asm.mixin.Mixin;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.HashMap;
 import java.util.function.Supplier;
+
+import static com.fasterxml.jackson.databind.type.LogicalType.Map;
 
 @Mixin(GenericContainerScreen.class)
 public abstract class ChestScreenMixin {
     @Inject(method = "render", at = @At("TAIL"))
     private void onRender(DrawContext context, int mouseX, int mouseY, float deltaTicks, CallbackInfo ci) {
-        System.out.println("Сундук открыт!");
+        //System.out.println("Сундук открыт!");
         GenericContainerScreen screen = (GenericContainerScreen) (Object) this;
-        Screens.getButtons(screen).add(
-                Screens.getButtons(screen).add(
-                        //ButtonWidget.builder(n);
-                );
-        )
+        Screens.getButtons(screen).add(new ButtonWidget.Builder(Text.literal("E"), (button) -> {
+
+            //GenericContainerScreenHandler handler = screen.getScreenHandler();
+            StorageCounter.LOGGER.info(ChestInventoryInteraction.getMap(screen.getScreenHandler().getInventory(), new HashMap<String, Integer>()).toString());
+        }).position(5, 5).size(15, 15).build());
     }
 }
